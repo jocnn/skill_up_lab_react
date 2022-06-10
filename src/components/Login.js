4a2fcbb5c414bdcaf38463f08c818d5ae970daf8
jocnn <ijann_@outlook.com>
2022-06-10 17:45:51 -0300

    validando formulario, usando useState

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,51 @@
+import { useState } from "react"
+
 const Login = () => {
+
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  // eslint-disable-next-line no-useless-escape
+  const regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if ([email, password].includes('')) {
+      console.log('Todos los campos son obligatorios')
+      return
+    }
+    if (email !== '' && !regexEmail.test(email)) {
+      console.log('Debe ingresar un email válido')
+      return
+    }
+  }
+
   return (
     <>
       <h2>Formulario de Login</h2>
-      <form>
+      <form
+        onSubmit={handleSubmit}
+      >
         <label htmlFor="email">
           Correo Electrónico
           <input
             name="email"
             autoComplete="email"
-            type="email" />
+            type="email" 
+            value={email}
+            onChange={ e => setEmail(e.target.value) }
+          />
         </label>
         <label htmlFor="password">
           Contraseña
           <input 
             name="password" 
             autoComplete="current-password" 
-            type="password" />
+            type="password" 
+            value={password}
+            onChange={ e => setPassword(e.target.value) }
+          />
         </label>
         <input 
           type="submit" 
