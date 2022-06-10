6c4681423c05e3ee149a61fc80cc1855247e68d3
jocnn <ijann_@outlook.com>
2022-06-10 19:36:25 -0300

    se agrego react-toastify para el renderizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Login from "./components/Login";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
-      <Login />
+      <Login  />
+      <ToastContainer />
     </div>
   );
 }
