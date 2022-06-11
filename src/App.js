eb72668b5cb6da72d82116bc60d8223c632ce665
jocnn <ijann_@outlook.com>
2022-06-10 21:50:02 -0300

    configurando las rutas hacia los componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Login from "./components/Login";
+import List from "./components/List";
+
 function App() {
   return (
-    <div>
-      <Login  />
+    <>
+      <Routes>
+        <Route>
+          <Route path='/' >
+            <Route index path='/' element={ <Login /> } />
+            <Route index path='/listado' element={ <List /> } />
+          </Route>
+
+        </Route>
+      </Routes>
       <ToastContainer />
-    </div>
+    </>
   );
 }
 
