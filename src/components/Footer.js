0a253bb5facccd9953885b4ec9c5931518e0d765
jocnn <ijann_@outlook.com>
2022-06-12 08:21:21 -0300

    se creo el componente pie de página

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="http://instagram.com" rel="noopener noreferrer">IG</a>
+          </li>
+        </ul>
+      </nav>
+      <p>Copyright Alkemy Challenger</p>
+    </footer>
+  )
+}
+
+export default Footer
