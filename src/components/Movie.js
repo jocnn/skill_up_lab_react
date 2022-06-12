62a8d6a2d7578121774df1ccc4138dfd13b0fc93
jocnn <ijann_@outlook.com>
2022-06-12 15:07:17 -0300

    asegurando el acceso al endpoint

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 
 const Movie = ({ movie }) => {
   
+  const token = sessionStorage.getItem('token')
+
   return (
     <>
-      <div className="col-3">
-        <div className="card m-2">
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="card-img-top" alt="poster path movie" />
-          <div className="card-body">
-            <h5 className="card-title">{movie.title}</h5>
-            <p className="card-text">{movie.overview.substring(0, 100)}</p>
-            <Link to={`/detalle?id=${movie.id}`} className="btn btn-primary">View Details</Link>
+    {
+      !token ? <Navigate to={'/'} /> : 
+      (
+        <div className="col-3">
+          <div className="card m-2">
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="card-img-top" alt="poster path movie" />
+            <div className="card-body">
+              <h5 className="card-title">{movie.title}</h5>
+              <p className="card-text">{movie.overview.substring(0, 100)}</p>
+              <Link to={`/detalle?id=${movie.id}`} className="btn btn-primary">View Details</Link>
+            </div>
           </div>
         </div>
-      </div>
+      )
+    }
     </>
   )
 }
