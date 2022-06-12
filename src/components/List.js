4df21a1f653fe112a83c720eeb78f49874a5ff1e
jocnn <ijann_@outlook.com>
2022-06-12 09:38:21 -0300

    protegiendo ruta en listado

--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,27 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 const List = () => {
-  
-  const navigate = useNavigate()
-  
-  useEffect(() => {
-    const verificarToken = () => {
-      const token = localStorage.getItem('token')
-      if (token === null) {
-        navigate('/')
-      }
-    }
-    verificarToken()
-  }, [navigate])
-    
+
+  const token = localStorage.getItem('token')
 
   return (
     <>
-      <h1>List</h1>
+      {
+        !token ? <Navigate to={'/'} /> : (
+          <div className="row">
+            <div className="col-3">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <Link to="/" className="btn btn-primary">Go somewhere</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
     </>
   )
 }
