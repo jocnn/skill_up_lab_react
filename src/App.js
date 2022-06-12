03b3256660866e603ba96420c71f4a6125893389
jocnn <ijann_@outlook.com>
2022-06-12 08:22:40 -0300

    se importo el layout y se creo la ruta de contacto

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Layout from "./components/Layout";
 import Login from "./components/Login";
 import List from "./components/List";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <>
+    <Layout>
       <Routes>
         <Route>
           <Route path='/' >
             <Route index path='/' element={ <Login /> } />
             <Route index path='/listado' element={ <List /> } />
+            <Route index path='/contacto' element={ <Contact /> } />
           </Route>
-
         </Route>
       </Routes>
       <ToastContainer />
-    </>
+    </Layout>
   );
 }
 
