af8582998c026ee33956896084a2da968112b910
jocnn <ijann_@outlook.com>
2022-06-13 08:52:23 -0300

    se creo el detalle de película

--- src/components/DetailMovie.js
+++ src/components/DetailMovie.js
@@ -1,7 +1,60 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Navigate, useParams } from "react-router-dom"
+
 const DetailMovie = () => {
+
+  const [ movie, setMovie ] = useState(null)
+
+  const token = sessionStorage.getItem('token')
+  const { movieID } = useParams()
+
+  useEffect(() => {
+    axios(`https://api.themoviedb.org/3/movie/${movieID}?api_key=ef2fc8163ab84be521bd136dab63ff24&language=es-ES`)
+      .then( response => {
+        const apiData = response.data
+        setMovie(apiData)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }, [movieID])
+
   return (
     <>
-      <h1>DetailMovie</h1>
+      {
+        !token ? <Navigate to={'/'} /> : 
+        (
+          <>
+            {
+              movie && 
+                <>
+                  <h1>Titulo: {movie.title}</h1>
+                  <div className="row">
+                    <div className="col-4">
+                      <img
+                        className="img-fluid" 
+                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
+                        alt="backdrop path" />
+                    </div>
+                    <div className="col-8">
+                      <h5>Fecha de Estreno: {movie.release_date}</h5>
+                      <h5>Reseña</h5>
+                      <p>{movie.overview}</p>
+                      <h5>Géneros:</h5>
+                      <ul>
+                        {
+                          movie.genres.map( genre => <li key={genre.id}>{genre.name}</li> )
+                        }
+                      </ul>
+                    </div>
+                  </div>                
+                </>
+            }
+          </>
+        )
+      }
     </>
   )
 }
