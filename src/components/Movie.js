6a559378bb45a1085c2e8bb1b5723e3c8cc4083a
jocnn <ijann_@outlook.com>
2022-06-14 17:32:34 -0300

    se agrego un button y se uso el prop

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,6 @@
 import { Link, Navigate } from "react-router-dom"
 
-const Movie = ({ movie }) => {
+const Movie = ({ movie, addOrRemoveFavorite }) => {
   
   const token = sessionStorage.getItem('token')
 
@@ -12,6 +12,11 @@ const Movie = ({ movie }) => {
         <div className="col-3">
           <div className="card m-2">
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="card-img-top" alt="poster path movie" />
+            <button 
+              data-movie-id={movie.id}
+              className="favorite-btn"
+              onClick={ addOrRemoveFavorite }
+            >&#x1F5A4;</button>
             <div className="card-body">
               <h5 className="card-title">{movie.title}</h5>
               <p className="card-text">{movie.overview.substring(0, 100)}</p>
